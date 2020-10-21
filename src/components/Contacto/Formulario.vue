26ece6552166783a4e1e15d42c09fc19775b23b0
Jorge Villarreal <jorge.vi96@gmail.com>
2020-10-21 18:35:47 -0500

    Commit prueba contacto

---
+++ src/components/Contacto/Formulario.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container mt-3">
+    <p>Contactanos !</p>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">Nombre</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">Correo@</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">Tel +</span>
+      <input
+        type="text"
+        class="form-control"
+        aria-label="Username"
+        aria-describedby="basic-addon1"
+      />
+    </div>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">Motivo</span>
+      </div>
+      <textarea class="form-control" aria-label="Motivo"></textarea>
+    </div>
+    <button type="button" class="btn btn-warning">Enviar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hover: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,38 +1,37 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="align-middle text-center">
-                    <div class="mt-5 "> 
-                        <div class="w-100 header__text mt-5">
-                        <h1 class="mt-5 pt-5">Que se te antoja?</h1>     
-                        </div>
-                    </div>
-                </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <div class="align-middle text-center">
+          <div class="mt-5 ">
+            <div class="w-100 header__text mt-5">
+              <h1 class="mt-5 pt-5">Que se te antoja?</h1>
             </div>
-            <div class="col-12 col-md-6">
-                <div class="text-center mb-4">
-                    <img src="../../assets/img/p-bg.png" alt="" style="max-width: 100%">
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+      <div class="col-12 col-md-6">
+        <div class="text-center mb-4">
+          <img src="../../assets/img/p-bg.png" alt="" style="max-width: 100%" />
+        </div>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            hover: false
-        }
-    }
-}
+  data() {
+    return {
+      hover: false
+    };
+  }
+};
 </script>
 
 <style scoped>
 .header__text p {
-    font-weight: 300;
+  font-weight: 300;
 }
 
 h1 {
@@ -41,6 +40,4 @@ h1 {
   color: #fff4f4;
   text-shadow: 0px 4px 4px rgba(1, 1, 1, 0.76);
 }
-
-
 </style>
