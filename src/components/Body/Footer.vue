dde9721ff636075c4413ce5f2310c10e2fbdaeaa
JesCvz <59677335+JesCvz@users.noreply.github.com>
2020-11-23 11:28:18 -0600

    Update Footer.vue

--- src/components/Body/Footer.vue
+++ src/components/Body/Footer.vue
@@ -5,10 +5,9 @@
             <a class="text-white mr-5" href="">AVISO DE PRIVACIDAD</a>
             <a class="text-white  mr-5" href="">AVISO  LEGAL</a>
             © 2020 Copyright:
-            <a  href="https://mdbootstrap.com/"> Pasteleria Yummy</a>
-            <a class="text-white ml-4" href=""><img src="../../assets/img/fb.png" alt=""></a>
-            <a class="text-white ml-4" href=""><img src="../../assets/img/instagram.png" alt=""></a>
-            <a class="text-white ml-4" href=""><img src="../../assets/img/twitter.png" alt=""></a>
+            <a class="text-white ml-4" > Pasteleria Yummy</a>
+            <a class="text-white ml-4" href="https://www.facebook.com/Yummy-Pasteleria-103286231592222" target="_blank"><img src="../../assets/img/fb.png" alt=""></a>
+            <a class="text-white ml-4" href="https://instagram.com/yummypasteleria.mty?igshid=stxx7947ld54" target="_blank"><img src="../../assets/img/instagram.png" alt=""></a>
         </div>
     </footer>
 </template>
